a9825cdc2cc1759a2881e0f99620e04e3801f2f7
Isaac Chabon <ichabon@me.com>
2020-11-18 12:23:32 -0500

    update styles

--- scripts/project.js
+++ scripts/project.js
@@ -5,6 +5,8 @@ const description = document.querySelector('.project-description');
 
 
 window.addEventListener('scroll', () => {
+
+  console.log(hero.getBoundingClientRect());
   const scrollRemaining = Math.round(info.getBoundingClientRect().y / window.innerHeight * 100) / 100;
   hero.style.opacity = scrollRemaining;
   info.classList.toggle('top', scrollRemaining <= 0);
