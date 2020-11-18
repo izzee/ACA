e7ce118185f92866942a8ae1a16753ce14bea259
Isaac Chabon <ichabon@me.com>
2020-11-18 13:04:59 -0500

    set up mobile design

--- scripts/project.js
+++ scripts/project.js
@@ -5,10 +5,10 @@ const description = document.querySelector('.project-description');
 
 
 window.addEventListener('scroll', () => {
-
-  console.log(hero.getBoundingClientRect());
-  const scrollRemaining = Math.round(info.getBoundingClientRect().y / window.innerHeight * 100) / 100;
-  hero.style.opacity = scrollRemaining;
-  info.classList.toggle('top', scrollRemaining <= 0);
-
+  if(window.innerWidth > 768){
+    console.log(hero.getBoundingClientRect());
+    const scrollRemaining = Math.round(info.getBoundingClientRect().y / window.innerHeight * 100) / 100;
+    hero.style.opacity = scrollRemaining;
+    info.classList.toggle('top', scrollRemaining <= 0);
+  }
 })
